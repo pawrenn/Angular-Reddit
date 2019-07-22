62e2cab8000169a0a4b05d65c3c29d915bc6cbdb
pawrenn <38671273+pawrenn@users.noreply.github.com>
2019-07-21 22:21:49 -0500

    First commit

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { TouchSequence } from 'selenium-webdriver';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass='row';
+  article: Article;
+
+  constructor() {
+    this.article = new Article('Angular','http://angular.io',10);
+   }
+
+   voteUp(): boolean{
+     this.article.voteUp();
+     return false;
+   }
+
+   voteDown(): boolean{
+     this.article.voteDown();
+     return false;
+   }
+
+  ngOnInit() {
+  }
+
+}
